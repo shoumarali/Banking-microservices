e748f5d4266659dbbd6b572d17659a5e0e8457bc
shoumarali <alialakbarshoumar610@gmail.com>
2025-04-24 14:42:57 +0300

    docs(loan-service): add documentation

--- loan-service/src/main/java/com/ashoumar/loan_service/LoanServiceApplication.java
+++ loan-service/src/main/java/com/ashoumar/loan_service/LoanServiceApplication.java
@@ -1,9 +1,17 @@
 package com.ashoumar.loan_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "Bank Loans microservice REST API Documentation",
+				version = "v1")
+)
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoanServiceApplication {
